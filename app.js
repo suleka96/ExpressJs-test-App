d6122984d39d789857abb56f529dd554b1108cb5
suleka96 <sulekahelmini96comp@gmail.com>
2018-01-24 23:07:22 +0530

    more functionality tested

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require ('path');
+var expressValidator =require('express-validator');
 
 var app = express();
 
@@ -24,11 +25,34 @@ app.use(bodyParser.urlencoded({extended:false}))
 //set static path
 app.use(express.static(path.join(__dirname,'public')))
 
+//global Variables
+app.use(function(req,res,next){
+    res.locals.errors =null;
+    next();
+});
+
+//Express Validator Middleware (to be put)
+
+
+
+
+
 //json string
-var person ={
+var people =[
+    {
     name: 'sul',
     age: 30
-}
+    },
+    {
+        name: 'jil',
+        age: 40
+    },
+    {
+        name: 'max',
+        age: 39
+    }
+
+]
 
 //route handler
 // app.get('/',function(req,res){
@@ -37,9 +61,39 @@ var person ={
 
 //route handler - rendering view engine views
 app.get('/',function(req,res){
+    var title ='customer';
     res.render('index',{
-        
+        title:'customer',
+        users : people
     });
+});
+
+app.post('/users/add',function(req,res){
+    console.log("form submitted");
+
+    // setting express validation rules of epress-validator
+    request.checkBody('name','Name is Required').notEmpty;
+    request.checkBody('age','Age is Required').notEmpty;
+
+    var errors = req.validationErrors();
+
+    if(errors){
+        res.render('index',{
+            title:'customer',
+            users : people,
+            error: errors
+        });
+    }
+    else{
+        var newUser= {
+            name: req.body.name,
+            age: req.body.age
+        }
+        console.log(Done);
+
+    }
+
+    
 });
 
 //route handler
